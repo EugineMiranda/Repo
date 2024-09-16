17ee0079e294b51888f8b73b4367963760d73626
EugineMiranda <kenjiesuzuki123@gmail.com>
2024-09-16 11:11:01 +0800

    Add or update build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" // this version matches your Kotlin version
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    alias(libs.plugins.kotlin.android) apply false // this version matches your Kotlin version
 }
 
+buildscript {
+    repositories {
+        maven("https://jitpack.io")
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Ensure this matches your Kotlin version
+        classpath(libs.kotlin.gradle.plugin)
+        // You may need other classpath dependencies here
+    }
+}
